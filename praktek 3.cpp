42a8dfba307d1a6684ae8e3c9ed3ae25c1f17bd7
besti <bestitantiyar17@gmail.com>
2017-11-08 21:13:12 +0800

    Menghitung dan menampilkan selisih absolute

---
+++ praktek 3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	int x,y,z;
+	
+	cout <<"Menghitung Selisih Absolute"<<endl;
+	cout<<"Masukkan Nilai X : ";
+	cin>>x;
+	cout <<"Masukkan Nilai Y : ";
+	cin>>y;
+	
+	if (x>y){
+		z=x-y;
+	}
+	else if (y>x){
+		z=y-x;
+	}
+	
+	cout <<"Hasil : "<<z;
+	
+	
+	
+}
+
+
